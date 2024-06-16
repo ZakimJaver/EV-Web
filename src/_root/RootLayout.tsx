9af790fa9a6782183e8a8f81a620caca9d482742
ZakimJaver <javerzakim08@gmail.com>
2024-06-16 12:26:53 -0400

    EW-22 Add shadcn library and update root layout with button to all pages

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,15 +1,51 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, Link } from 'react-router-dom'
 
 const RootLayout = () => {
-  return (
-    <div className='w-full md:flex'>
-        <h1>RootLayout</h1>
-        <section>
-            <Outlet />
-        </section>
-    </div>
-  )
+
+    return (
+        <div className='w-full'>
+            <h1>RootLayout</h1>
+
+            <div className='md:flex flex-col gap-2'>
+
+                <Link to="/browser">
+                    <Button
+                        onClick={() => console.log("clicked 1")}>ZEV Browser</Button>
+                </Link>
+                <Link to="/ev-calculator">
+                    <Button
+                        onClick={() => console.log("clicked 2")}>EV Savings Calculator</Button>
+                </Link>
+                <Link to="/">
+                    <Button
+                        onClick={() => console.log("clicked 3")}>Home</Button>
+                </Link>
+                <Link to="/incentives">
+                    <Button
+                        onClick={() => console.log("clicked 4")}>Incentives</Button>
+                </Link>
+                <Link to="/industry-map">
+                    <Button
+                        onClick={() => console.log("clicked 5")}>Industry Map</Button>
+                </Link>
+                <Link to="/service-provider">
+                    <Button
+                        onClick={() => console.log("clicked 6")}>Service Provider</Button>
+                </Link>
+                <Link to="/sustainability-tracker">
+                    <Button
+                        onClick={() => console.log("clicked 7")}>Sustainability Tracker</Button>
+                </Link>
+
+            </div>
+
+            <section className='bg-yellow-500 m-8'>
+                <Outlet />
+            </section>
+        </div>
+    )
 }
 
 export default RootLayout
