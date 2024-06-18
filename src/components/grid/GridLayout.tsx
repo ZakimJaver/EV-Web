e255e537844a1779c393f5938d443c37a3ec1921
ZakimJaver <javerzakim08@gmail.com>
2024-06-18 19:51:11 -0400

    EW-23 add font to project

--- src/components/grid/GridLayout.tsx
+++ src/components/grid/GridLayout.tsx
@@ -12,9 +12,9 @@ type RoutePaths = {
 const GridLayout = ({ items }: GridLayoutProps) => {
     if (!items) return
     return (
-        <div className="flex flex-col h-screen w-screen">
-            <div className="h-24 bg-black">
-                <h1 className="text-white">Menu Items</h1>
+        <div className="flex flex-col h-screen w-screen titilliumWeb">
+            <div className="h-16 bg-black">
+                <h1 className="text-white ">Menu Items</h1>
             </div>
             <div className='grid-container'>
                 {items?.map((item: RoutePaths) => {
@@ -28,15 +28,3 @@ const GridLayout = ({ items }: GridLayoutProps) => {
 }
 
 export default GridLayout
-/*
-
-
-                <div className='grid-card'>
-                    {items?.map((item: RoutePaths) => {
-                        return (
-                            <MenuCard key={item.label} route={item.route} label={item.label} />
-                        )
-                    })}
-                </div>
-
-*/
